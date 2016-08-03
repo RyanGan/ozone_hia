c1eacbd32d599269d59968533c9ecd266f336562
RyanGan <ryan.william.gan@gmail.com>
2016-08-03 14:05:19 -0600

    start of new script.

---
+++ monte_carlo_strata_loops.R
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Title: Monte Carlo approacc to estimating excess asthma emergency depart
+#        visits due to wildfire-generated ozone; stratum-specific estimates
+# Date Created: 8/3/16
+# ------------------------------------------------------------------------------
+
+# No libraries need as we use the base functions
+
+# check the dataframes Jake created
+list.files('./data')
