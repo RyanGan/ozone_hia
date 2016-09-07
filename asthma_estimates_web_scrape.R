74979d1e27fdf8f1c680b9603bf4fd31ae2a325d
RyanGan <ryan.william.gan@gmail.com>
2016-09-07 16:35:01 -0600

    Loop to create empty dataframe. Tomorrow I need to work on loop to populate dataframe with web scrape.

--- asthma_estimates_web_scrape.R
+++ asthma_estimates_web_scrape.R
@@ -7,11 +7,48 @@
 # load rvest library for web scrapping
 library(rvest)
 
-# Scrap CDC BRFSS for childhood asthma prevalences -----------------------------
+# General process
+# 1. Bring in each BRFSS HTML table
+# 2. Find the prevalence number and calculated standard error for all US and 
+#    each state for all the years available
+# 3. Run meta-analysis for all US then each US to get a pooled estimate and se
 
-# define URL path
-url <- paste0("http://www.cdc.gov/asthma/brfss/05/child/lifetime/",
-              "tableL1.htm#modalIdString_CDCTable_0")
+# state list
+state_abbrev <- c("AL", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA",
+                  "ID", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
+                  "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+                  "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN",
+                  "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
+
+# list of BRFSS URLs to loop through
+year_url <- c("2013", "2012", "2011", "2010", "09", "08", "07", "06", "05")
+
+
+year <- c("2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005")
+
+yearly_prev_se_est <- data.frame()
+
+for (i in 1:length(year)){
+
+year_var <- replicate(length(state_abbrev), year[i])
+prev_n_est <- replicate(length(state_abbrev), NA)
+prev_se_est <- replicate(length(state_abbrev), NA)
+
+df <- data.frame(year_var, state_abbrev, prev_n_est, prev_se_est)
+
+yearly_prev_se_est <- rbind(yearly_prev_se_est, df)
+
+}
+# create an empty dataframe to fill with prevalence n and se 
+
+
+# Loop to scrape CDC BRFSS for childhood asthma prevalences --------------------
+
+
+
+# define URL path for current asthma prev tables
+url <- paste("http://www.cdc.gov/asthma/brfss/", year_url, "/child/tableC1.htm",
+             sep = "")
 
 # Use rvest package to extract the table
 asthma_html <- url %>% read_html() %>% 
@@ -19,25 +56,29 @@ asthma_html <- url %>% read_html() %>%
   html_table(fill = T, header = T, dec = '.')
 
 # convert html table to data.frame
-asthma_table <- data.frame(asthma_html)[1:24,] # exclude Puerto Rico
-asthma_table
+asthma_table <- data.frame(asthma_html) 
+# exclude Puerto Rico, Alaska, and Hawaii
+asthma_table <- subset(asthma_table, State != "PR" & State != "Territories"
+                       & State != "Territory" & State != "HI" & State != "AK")
 
 # output estimate names
 estimate_name <- unlist(asthma_table$State)
-estimate_name[1] <- "all_us"
+# Replace 'Total **' with 'US' label
+estimate_name[1] <- "US"
 
 # I want the prevalence in estimated numbers
 # subset prevalence number
 prev_n <- unlist(asthma_table$Prevalence.Number.)
-prev_n <- as.numeric(gsub(",", "", prev_test)) # seperate out commas
+prev_n <- as.numeric(gsub(",", "", prev_n)) # seperate out commas
 
 # split the confidence bounds by '-'
-bounds <- unlist(strsplit(asthma_table$X95..CI...Number., split = "[-]"))
+bounds <- unlist(strsplit(asthma_table$X95..CI...Number., split = "-"))
 bounds
 
 # subset out lower bound vector and convert to numeric
 lower <- bounds[c(T,F)]
-count_95lower <- as.numeric(gsub(",", "", (gsub("(", "", lower)))) 
+lower # i need to escape left '(', need '\\('
+count_95lower <- as.numeric(gsub(",", "", (gsub("\\(", " ", lower)))) 
 # subset out upper bound vector and convert to numeric
 upper <- bounds[c(F,T)]
 count_95upper <- as.numeric(gsub(",", "", (gsub(")", "", upper))))
@@ -46,6 +87,5 @@ state_prev_counts <- data.frame(estimate_name, prev_n, count_95lower, count_95up
 # calculate SE for each (might not be exact as BRFSS likely uses weights for their SE)
 state_prev_counts$se_n <- (state_prev_counts$count_95upper - state_prev_counts$prev_n)/1.96
 
-us_prev <- subset(state_prev_counts, estimate_name == "all_us")
-str(state_prev_counts)
-head(state_prev_counts)
+us_prev <- subset(state_prev_counts, estimate_name == "US")
+
