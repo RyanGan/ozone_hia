d55d0f9e464ee5f6cd6313c143f67e86f62f230f
RyanGan <ryan.william.gan@gmail.com>
2016-09-07 13:36:25 -0600

    a little closer.

--- asthma_estimates_web_scrape.R
+++ asthma_estimates_web_scrape.R
@@ -8,20 +8,44 @@
 library(rvest)
 
 # Scrap CDC BRFSS for childhood asthma prevalences -----------------------------
+
+# define URL path
 url <- paste0("http://www.cdc.gov/asthma/brfss/05/child/lifetime/",
               "tableL1.htm#modalIdString_CDCTable_0")
 
-# table is in there, but I need it in a usable dataframe
-asthma_2012 <- url %>% read_html() %>% 
+# Use rvest package to extract the table
+asthma_html <- url %>% read_html() %>% 
   html_nodes('table') %>% 
   html_table(fill = T, header = T, dec = '.')
 
+# convert html table to data.frame
+asthma_table <- data.frame(asthma_html)[1:24,] # exclude Puerto Rico
+asthma_table
 
-head(asthma_2012)
+# output estimate names
+estimate_name <- unlist(asthma_table$State)
+estimate_name[1] <- "all_us"
 
-# data.frame does the conversion
-asthma_table <- data.frame(asthma_2012)
-asthma_table_test <- as.numeric(asthma_table$Prevalence.Number.)
+# I want the prevalence in estimated numbers
+# subset prevalence number
+prev_n <- unlist(asthma_table$Prevalence.Number.)
+prev_n <- as.numeric(gsub(",", "", prev_test)) # seperate out commas
 
-test <- unlist(strsplit(asthma_table$X95..CI...Number., split = "[-]"))
-test
+# split the confidence bounds by '-'
+bounds <- unlist(strsplit(asthma_table$X95..CI...Number., split = "[-]"))
+bounds
+
+# subset out lower bound vector and convert to numeric
+lower <- bounds[c(T,F)]
+count_95lower <- as.numeric(gsub(",", "", (gsub("(", "", lower)))) 
+# subset out upper bound vector and convert to numeric
+upper <- bounds[c(F,T)]
+count_95upper <- as.numeric(gsub(",", "", (gsub(")", "", upper))))
+
+state_prev_counts <- data.frame(estimate_name, prev_n, count_95lower, count_95upper)
+# calculate SE for each (might not be exact as BRFSS likely uses weights for their SE)
+state_prev_counts$se_n <- (state_prev_counts$count_95upper - state_prev_counts$prev_n)/1.96
+
+us_prev <- subset(state_prev_counts, estimate_name == "all_us")
+str(state_prev_counts)
+head(state_prev_counts)
